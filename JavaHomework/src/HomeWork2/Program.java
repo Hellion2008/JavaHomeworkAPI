98a04c98e034cb259e0fd89b62fcedbe2a6197ca
Sergey <semakin.s.v@gmail.com>
2023-04-18 16:41:31 +0300

    added task1

--- JavaHomework/src/HomeWork2/Program.java
+++ JavaHomework/src/HomeWork2/Program.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        task0jewels();
+//        task0jewels();
+        task1shuffleString();
     }
 
     static String findJewelsInStones(String jewels, String stones) {
@@ -28,6 +29,20 @@ public class Program {
             String stones  = scanner.nextLine();
             System.out.println(findJewelsInStones(jewels, stones));
         }
+    }
+
+    static String shuffle(final String s, final int[] index) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < index.length; i++) {
+            stringBuilder.append(s.charAt(index[i]));
+        }
+        return stringBuilder.toString();
+    }
+
+    static void task1shuffleString(){
+        String s = "abcdefghij";
+        int[] index = {2,1,0,4,5,8,7,9,3,6};
+        System.out.println(shuffle(s,index));
     }
 
 }
