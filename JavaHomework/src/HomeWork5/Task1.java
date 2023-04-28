fc244fb65b5c2ad61c4a020633a6b6ac32c23c1c
Sergey <semakin.s.v@gmail.com>
2023-04-28 13:19:17 +0300

    added task1 hw5

---
+++ JavaHomework/src/HomeWork5/Task1.java
@@ -0,0 +1,16 @@
+package HomeWork5;
+
+import java.time.Period;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Phones phones = new Phones();
+        phones.add("Petrov", 13246);
+        phones.add("Korovev", 79851468);
+        phones.add("Petrov", 454123);
+        phones.add("Kukushkin", 4188845);
+        System.out.println(phones.print());
+        System.out.println(phones.getNumber("Petrov"));
+        System.out.println(phones.getByNumber(79851468));
+    }
+}
